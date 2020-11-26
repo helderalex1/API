2e8ada90fe6ca2ab9299b3279afa033c2477e900
helder <2190785@my.ipleiria.pt>
2020-11-26 22:42:53 +0000

    PWSAP-78 #DONE
    autenticação por token

--- API/config/web.php
+++ API/config/web.php
@@ -15,9 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'pbre6vODj8kUE6F3kiBryLpdbvE6drh0',
-            /*'parsers' => [
+            'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
-            ]*/
+            ]
 
         ],
         'cache' => [
